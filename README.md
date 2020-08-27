e6849296f44c08274c9c4494fcde41a46a91a56a
Lis <lisazevedo3@gmail.com>
2020-08-27 17:46:13 -0300

    Add exercises 6 and 7

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # python-workbook-1
 Team 3
+
+- [ ] Exercise 2 
+- [ ] Exercise 5
+- [x] Exercise 6
+- [x] Exercise 7
+- [ ] Exercise 8
+- [ ] Exercise 11
+
---
+++ exercise-7.py
@@ -0,0 +1,5 @@
+import random
+
+def random_password(): return ''.join(chr(random.randrange(33, 126, 2)) for _ in range(random.randrange(7, 10, 2)))
+
+print(random_password())
